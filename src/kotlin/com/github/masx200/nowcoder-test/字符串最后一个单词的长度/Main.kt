4f90297b36ef423943060243a0048af8e5c44f82
masx200 <34191203+masx200@users.noreply.github.com>
2024-05-10 00:19:38 +0800

    字符串最后一个单词的长度

---
+++ src/kotlin/com/github/masx200/nowcoder-test/字符串最后一个单词的长度/Main.kt
@@ -0,0 +1,14 @@
+package kotlin.com.github.masx200.`nowcoder-test`.字符串最后一个单词的长度
+
+import java.util.Scanner
+
+
+fun main(args: Array<String>) {
+    val `in` = Scanner(System.`in`)
+
+    while (`in`.hasNextLine()) {
+        val a = `in`.nextLine()
+
+        println(a.split(" ").last().length)
+    }
+}
