d2680e349a64b1f9f9bb0b35c895bda2d8e68e94
masx200 <34191203+masx200@users.noreply.github.com>
2024-05-10 18:21:54 +0800

    字符串分隔

---
+++ src/字符串分隔/Main.kt
@@ -0,0 +1,27 @@
+package com.github.masx200.`nowcoder-test`.字符串分隔
+
+import java.util.Scanner
+
+
+fun main(args: Array<String>) {
+    val `in` = Scanner(System.`in`)
+
+    while (`in`.hasNextLine()) {
+        val a = `in`.nextLine()
+
+        for (i in stringSeparation(a)) {
+            println(i)
+        }
+    }
+}
+
+fun stringSeparation(
+    a: String,
+): Sequence<String> = sequence {
+    if (a.length > 8) {
+        yield(a.slice(0..7))
+        yieldAll(stringSeparation(a.slice(8..a.lastIndex)))
+    } else {
+        yield(a + "0".repeat(8 - a.length))
+    }
+}
