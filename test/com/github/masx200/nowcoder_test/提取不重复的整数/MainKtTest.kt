35a633fca1603a399a21de7f930609cb0d9e1879
masx200 <34191203+masx200@users.noreply.github.com>
2024-05-13 16:35:19 +0800

    Create MainKtTest.kt

---
+++ test/com/github/masx200/nowcoder_test/提取不重复的整数/MainKtTest.kt
@@ -0,0 +1,19 @@
+package com.github.masx200.nowcoder_test.提取不重复的整数
+
+import com.github.masx200.`nowcoder-test`.utils.MockTest
+import com.github.masx200.`nowcoder-test`.提取不重复的整数.main
+import kotlin.test.assertEquals
+
+class MainKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun Testmain() {
+        val result = MockTest(
+            "9876673",
+        ) {
+            main(arrayOf())
+        }
+        assertEquals("37689"+"\r\n", result[0])
+    }
+
+}
