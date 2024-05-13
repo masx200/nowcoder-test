53d04a3b396ff07fb9e7f216d5118e8cd9964c15
masx200 <34191203+masx200@users.noreply.github.com>
2024-05-13 12:33:31 +0800

    Update Main.kt

--- src/质数因子/Main.kt
+++ src/质数因子/Main.kt
@@ -5,15 +5,16 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val read = Scanner(System.`in`)
     while (read.hasNextLine()) {
-        var a = read.nextLine().toInt()
-        val factors = mutableListOf<Int>()
-        var i = 2
+        var a = read.nextLine().toLong()
+        val factors = mutableListOf<Long>()
+        var i = 2L
         while (i <= a) {
-            if (a % i == 0) {
+            if (a % i == 0L) {
                 a /= i
                 factors.add(i)
             } else {
-                i++
+                if (i > a / i) i = a
+                else i++
             }
         }
         println(factors.joinToString(" "))
